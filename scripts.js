dfa284a04b2d7aebf4701587da70d8522e7a6b44
RossieK <rosi.kirova96@gmail.com>
2021-01-02 21:53:06 +0200

    Add full screen button and functionality

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullScreenButton = player.querySelector('.full-screen');
 
 // Build functions
 function togglePlay() {
@@ -39,6 +40,16 @@ function scrub(e) {
     video.currentTime = scrubTime;
 }
 
+function openFullscreen() {
+    if (player.requestFullscreen) {
+        player.requestFullscreen();
+    } else if (player.webkitRequestFullscreen) { /* Safari */
+        player.webkitRequestFullscreen();
+    } else if (player.msRequestFullscreen) { /* IE11 */
+        player.msRequestFullscreen();
+    }
+}
+
 // Add event listeners
 video.addEventListener('click', togglePlay);
 toggle.addEventListener('click', togglePlay);
@@ -56,3 +67,5 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
+
+fullScreenButton.addEventListener('click', openFullscreen);
