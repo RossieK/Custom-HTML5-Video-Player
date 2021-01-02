57828ad656ad852e847cbce40fa00c16b4a296cd
RossieK <rosi.kirova96@gmail.com>
2021-01-02 21:35:54 +0200

    Add progress bar animation and scrub functionality

--- scripts.js
+++ scripts.js
@@ -28,13 +28,31 @@ function skip() {
 function handleRangeUpdate() {
     video[this.name] = this.value;
 }
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
 // Add event listeners
 video.addEventListener('click', togglePlay);
 toggle.addEventListener('click', togglePlay);
 
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
 
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
